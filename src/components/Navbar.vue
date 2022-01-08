457105fb63510da6450a2283013b1b25e63f990e
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2022-01-08 21:23:57 +0700

    create navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav id="nav">
+    <div class="logo">
+      <router-link :to="{ name: 'Home' }">Football Club</router-link>
+    </div>
+  </nav>
+</template>
+
+<style>
+#nav {
+  display: flex;
+  max-width: 100%;
+  padding: 1rem 2rem;
+  justify-content: center;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+
+.logo {
+  padding: 0 0.5rem;
+}
+
+.logo a {
+  font-weight: bold;
+  color: #3e4642;
+  font-size: 1.5rem;
+  text-decoration: none;
+}
+</style>
